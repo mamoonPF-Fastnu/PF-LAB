ad51af8f4d4a6cd55fb711e8747862e41a749982
mamoonPF-Fastnu <mamoonahmed1122@gmail.com>
2026-09-13 17:24:26 +0500

    Question 2

--- Lab 4/Question 2.cpp
+++ Lab 4/Question 2.cpp
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    float score;
+
+    printf("Enter confidence score: ");
+    scanf("%f", &score);
+
+    if (score < 0 || score > 100) {
+        printf("Invalid Score");
+    }
+    else if (score <= 49) {
+        printf("Low Confidence");
+    }
+    else if (score <= 79) {
+        printf("Moderate Confidence");
+    }
+    else {
+        printf("High Confidence");
+    }
+
+    return 0;
+}
